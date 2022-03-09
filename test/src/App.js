c3293e31573b12b634443c82b06fd24215cf2971
PayalNS <payalmahajan07@gmail.com>
2022-03-09 14:41:48 +0530

    Test Submitted

---
+++ test/src/App.js
@@ -0,0 +1,16 @@
+import Images from './Image';
+import './App.css';
+
+function App() {
+
+  return (
+
+    <div className="App">
+      
+
+    </div>
+  );
+
+}
+
+export default App;
---
+++ test/src/Image.js
@@ -0,0 +1,18 @@
+import React from "react";
+import img from './Images/Jwellery.jpg';
+
+
+function Images(){
+
+    return(
+
+        <div id="root">
+
+            <img src = {img} alt="Jwellery Image"/>
+
+        </div>
+
+    )
+}
+
+export default Images;
